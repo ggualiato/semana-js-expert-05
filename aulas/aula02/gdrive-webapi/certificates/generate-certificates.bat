f3796c9e3d51762549bc90566676535eda47023d
Giovanni Gualiato <ggualiato@gmail.com>
2021-09-10 01:20:07 -0300

    starting aula 02

---
+++ aulas/aula02/gdrive-webapi/certificates/generate-certificates.bat
@@ -0,0 +1,1 @@
+mkcert -key-file key.pem -cert-file cert.pem 0.0.0.0 localhost 127.0.0.1 ::1
---
+++ aulas/aula02/gdrive-webapi/src/routes.js
@@ -0,0 +1,46 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { logger } from "./logger.js";
+import { FileHelper } from "./fileHelper.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const defaultDownloadsFolder = resolve(__dirname, "../", "downloads");
+
+export default class Routes {
+  io;
+  constructor(downloadsFolder = defaultDownloadsFolder) {
+    this.downloadsFolder = downloadsFolder;
+    this.fileHelper = FileHelper;
+  }
+
+  setSocketInstance(io) {
+    this.io = io;
+  }
+
+  async defaultRoute(request, response) {
+    response.end("hello world");
+  }
+
+  async options(request, response) {
+    response.writeHead(204);
+    response.end();
+  }
+
+  async post(request, response) {
+    logger.info("POST");
+    response.end();
+  }
+
+  async get(request, response) {
+    const files = await this.fileHelper.getFilesStatus(this.downloadsFolder);
+
+    response.writeHead(200);
+    response.end(JSON.stringify(files));
+  }
+
+  handler(request, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    const chosen = this[request.method.toLowerCase()] || this.defaultRoute;
+    return chosen.apply(this, [request, response]);
+  }
+}
